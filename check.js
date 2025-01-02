050e631df92ebe861f086b487bd5d1e8a604eec7
Nova-Spectra-Games <nsg-group@nova-spectra-games.com>
2025-01-02 00:33:35 +0000

    push

--- check.js
+++
@@ -1,9 +0,0 @@
-fetch('https://uptime.betterstack.com/api/v2/monitors/123456789/response-times', {
-  method: 'GET',
-  headers: {
-    'Authorization': 'Bearer TchFg2LBzkmeZyDNtFM9nHSY'
-  }
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
